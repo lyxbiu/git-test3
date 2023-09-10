e95cd2046cbc35cdbf02c0934953ed0efd54590c
liuyx <liuyx@163.com>
2023-09-10 16:10:45 +0800

    push test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.printf("Hello Git4");
         System.out.printf("master test");
         System.out.printf("hot-fix test");
+        System.out.printf("push test");
     }
 }
