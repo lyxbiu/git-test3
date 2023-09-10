fb07555fedb047a2f1d249638f4347bbcbba11d8
liuyx <liuyx@163.com>
2023-09-10 15:38:29 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.printf("Hello Git");
+        System.out.printf("Hello Git2");
     }
 }
