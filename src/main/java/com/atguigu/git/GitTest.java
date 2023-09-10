5f1f144ebdbc1dae195022f585afd5db28fa5b03
liuyx <liuyx@163.com>
2023-09-10 15:39:40 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.printf("Hello Git");
         System.out.printf("Hello Git2");
+        System.out.printf("Hello Git3");
     }
 }
